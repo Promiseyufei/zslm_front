fc3bea36f772dfbc9e1ad744a1dbe4f91e853578
lishnalei <2686685661@qq.com>
2018-08-28 12:19:06 +0800

    引入font-awesome

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
                 <Navigation :collapse="isCollapse"></Navigation> 
             </el-aside>
             <el-container>
-                <el-header style="display:flex;align-items:center">
+                <el-header style="display:flex;align-items:center;justify-content:space-between;">
                     <el-button type="primary" v-if="isCollapse" @click="isCollapse = !isCollapse">展开导航</el-button>
                     <el-button type="primary" v-else @click="isCollapse = !isCollapse">关闭导航</el-button>
+                    
+                    <div>
+                        <i class="fa fa-user-circle fa-lg" aria-hidden="true"></i>
+                        <i class="fa fa-bell-o fa-lg" aria-hidden="true"></i>
+                        <i class="fa fa-share fa-lg" aria-hidden="true"></i>
+                    </div>
                 </el-header>
                 <el-main>
                     <router-view/>
@@ -57,5 +63,32 @@
       /* text-align: center; */
       /* line-height: 60px; */
     }
+
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 
 </style>
