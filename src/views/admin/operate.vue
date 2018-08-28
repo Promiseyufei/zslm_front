5c53fcb65403e31c3d9c6eecaf59250ad74e9a60
xuhong <1968543565@qq.com>
2018-08-28 14:38:06 +0800

    运营首页-上传banner

--- src/views/admin/operate.vue
+++ src/views/admin/operate.vue
@@ -12,27 +12,48 @@
           <div class="operateUpfiles">
             <div class="operateUpfilesLeft">
               <img src="" alt="">
-              <span>上传banner</span>
+              <div><i class="fa fa-cloud-upload"></i>上传banner</div>
             </div>
             <div class="operateUpfilesRight">
+              <div >
                 <div>
                   <el-upload
                     class="upload-demo"
-                    action="https://jsonplaceholder.typicode.com/posts/"
+                    action="/putPictrue"
                     :on-preview="handlePreview"
+                    :on-change="handleChange"
                     :on-remove="handleRemove"
                     :before-remove="beforeRemove"
-                    :on-exceed="handleExceed"
+                    :before-upload="beforeAvatarUpload"
+                    :on-success="handleAvatarSuccess"
                     :file-list="fileList">
                     <el-button size="small" type="primary">点击上传</el-button>
-                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                    <!-- <div slot="tip" class="el-upload__tip">只能上传jpg/png文件</div> -->
+                    <span class="el-upload__tip">当前以选择：</span>
                   </el-upload>
                 </div>
-                <div>
+                <div class="operateUpfilesRightImg">
                   <img :src="this.fileList[0].url" alt="">
                 </div>
+              </div>
+                
                 <div>
-                  Panner
+                  <el-form ref="form" :model="form" label-width="80px">
+                    <el-form-item label="图片名称">
+                      <el-input v-model="form.name" placeholder="输入图片名称"></el-input>
+                    </el-form-item>
+                    <el-form-item label="图片描述">
+                      <el-input v-model="form.message" placeholder="输入图片描述"></el-input>
+                    </el-form-item>
+                    <el-form-item label="图片URL">
+                      <el-input v-model="form.url" placeholder="输入图片URL"></el-input>
+                    </el-form-item>
+
+
+                    <el-form-item>
+                      <el-button type="primary" @click="onSubmit">上传</el-button>
+                    </el-form-item>
+                  </el-form>
                 </div>
             </div>
           </div>
@@ -215,23 +236,52 @@ export default {
         banner: [],
         index: "1",
         src: "",
-        fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
-
+        fileList: [{name: '', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+        form: {
+          name: '',
+          message: '',
+          url: ''
+        }
       }
     },
     methods:{
+        onSubmit() {
+          console.log('submit!');
+        },
         handleRemove(file, fileList) {
-          console.log(file, fileList);
+          var span = document.getElementsByClassName("el-upload__tip")[0];
+          span.style.display = "none";
+          this.fileList[0].url = 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100';
         },
         handlePreview(file) {
           console.log(file);
         },
-        handleExceed(files, fileList) {
-          this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-          // if () {};
+        handleAvatarSuccess(res, file) {
+          this.fileList[0].url = URL.createObjectURL(file.raw);
+        },
+        handleChange(file, fileList) {
+          var li = document.getElementsByClassName("el-upload-list")[0];
+          var span = document.getElementsByClassName("el-upload__tip")[0];
+          span.style.display = "block";
+          li.style.display = "block";
+          
+          this.fileList = fileList.slice(-1);
         },
         beforeRemove(file, fileList) {
           return this.$confirm(`确定移除 ${ file.name }？`);
+        },
+        beforeAvatarUpload(file) {
+          const isjpeg = file.type === 'image/jpeg';
+          const isPNG = file.type === 'image/png';
+          const isLt2M = file.size / 1024 / 1024 < 200;
+
+          if (!isjpeg&&!isPNG) {
+            this.$message.error('上传头像图片只能是 JPG/PNG 格式!');
+          }
+          if (!isLt2M) {
+            this.$message.error('上传头像图片大小不能超过 200MB!');
+          }
+          return isjpeg || isPNG && isLt2M;
         },
         handleSelect: function(key, keyPath) {
           // console.log(key, keyPath);
@@ -258,16 +308,35 @@ export default {
       let w = document.body.clientWidth;
       this.width = w - 120;
       this.getInformationType();
-
-      console.log(this.fileList[0].url);
     }
 }
 </script>
+<style>
+  /*
+  * 上传图片列表初始时不显示
+  */
+
+  .operateUpfilesRight .el-upload-list {
+    display: none;
+    margin-top: -10px;
+  }
+  
+</style>
 
 <style scoped>
 /*
 * 右边内容样式
 */
+.operateUpfilesRight .upload-demo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.operateUpfilesRight .el-upload__tip {
+  margin-left: 20px;
+  display: none;
+  float: right;
+}
 .operateUpfiles {
   border: 1px solid #e4e4e4;
   background-color: #fff;
@@ -277,13 +346,45 @@ export default {
 .operateUpfilesLeft {
   background-color: #fcfcfc;
   border-right: 1px solid #e4e4e4;
-  width: 160px;
+  width: 159px;
 }
-.operateUpfilesLeft>span {
-  /*background: url();*/
+.operateUpfilesLeft>div {
+  background: url(../../assets/img/point.png) no-repeat;
+  position: relative;
+  top: 10px;
+  left: 0;
+  width: 180px;
+  height: 50px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
 }
 .operateUpfilesRight {
   padding: 35px 90px 35px 80px;
+  width: 1170px;
+  display: flex;
+  justify-content: space-between;
 }
+.operateUpfilesRight .el-input {
+  width: 360px;
+}
+.operateUpfilesRight form {
+  margin-top: 50px;
+}
+.operateUpfilesRight button {
+  float: right;
+}
+.operateUpfilesRightImg {
+  width: 640px;
+  height: 170px;
+  overflow: hidden;
+  margin-top: 20px;
+}
+.operateUpfilesRightImg>img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
 </style>
 
