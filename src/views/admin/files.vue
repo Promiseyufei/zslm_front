b2e4e59ad7b510e581708a8f14164655f9a3352a
YouQiQin <1344869024@qq.com>
2018-08-29 09:24:29 +0800

    文件管理用mock获取数据

--- src/views/admin/files.vue
+++ src/views/admin/files.vue
@@ -25,19 +25,5 @@
 </script>
 
 <style scoped>
-	/*.top {
-		display: flex;
-		justify-content: space-between;
-	}*/
-	/*.screen {
-		font-weight: bold;
-		font-size: 14px;
-		color: #666;
-		font-family:'Tahoma';
-		background-color: #F3F3F3;
-		height: 30px;
-		border: 1px solid #E4E4E4;
-		padding: 15px 0 0 35px;
-		margin: -10px 0 0 20px;
-	}*/
+	
 </style>
