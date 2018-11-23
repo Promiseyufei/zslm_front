54dbe107653901b637b0ac182235f5566439b7f2
YouQiQin <1344869024@qq.com>
2018-11-23 11:43:06 +0800

    页面响应式

--- src/views/front/firstMenu/lookActivity.vue
+++ src/views/front/firstMenu/lookActivity.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
-        <div>
+        <!-- <div> -->
+            <!-- <div class="all">
+                <div class="q"></div>
+                <div class="w"></div>
+                <div class="e"></div>
+            </div> -->
             <!-- 找活动页面还没写～～～～～哇哈哈，我要举报，红斌是大笨蛋～ -->
-            <hearderBanner enName="COLLEGES" name="选院校"></hearderBanner>
-        </div>
+            <!-- <hearderBanner enName="COLLEGES" name="选院校"></hearderBanner> -->
+        <!-- </div> -->
         
     </div>
 </template>
@@ -28,7 +33,25 @@ export default {
 <style>
 </style>
 <style scoped>
-
-
+    /*.q {
+        width: 300px;
+        height: 100px;
+        background-color: blue;
+    }
+    .w {
+        width: 300px;
+        height: 100px;
+        background-color: black;
+        margin: 0 30px;
+    }
+    .e {
+        width: 300px;
+        height: 100px;
+        background-color: red;
+    }
+    .all {
+        width: 900px;
+        display: flex;
+    }*/
 
 </style>
--- src/views/front/firstMenu/selectCollege.vue
+++ src/views/front/firstMenu/selectCollege.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <div>
+        <div style="overflow-x:hidden;">
             <hearderBanner enName="COLLEGES" name="选院校"></hearderBanner>
-            <!-- <div class="selectCollege">
+            <div class="selectCollege">
                 <div class="selectCollegeSearch">
                     <el-input
                         placeholder="请输入关键字搜索"
@@ -10,10 +10,32 @@
                         v-model="searchContent">
                     </el-input>
                 </div>
-                <radioButton></radioButton>
-            </div> -->
-        </div>
-        
+            </div>
+            <radioButton></radioButton>
+            <!-- <div class="buttonCollege"></div> -->
+            <div class="diffeCollege">
+                <el-col :span="8">
+                    <el-card shadow="hover">
+                        <div class="collegePicture"></div>
+                        <div class="collegeInform" v-for="(m,index) in allMajorname" :key="index">
+                            <div class="forY">
+                                <div class="forX" v-for="(m,index) in allMajorname" :key="index">
+                                    <div class="specificInform">
+                                        <p class="international">国际</p>
+                                        <div class="moneyType">
+                                            <p><span><i class="el-icon-caret-bottom"></i></span>学费<span class="money">18.8万</span></p>
+                                            <p class="teach"><span><i class="el-icon-caret-bottom"></i></span>授课语言<span class="type">英文</span></p>
+                                        </div>
+                                        <p class="interval"><span><i class="el-icon-caret-bottom"></i></span>授课语言<span class="bold">全日制共100名</span></p>
+                                        <p class="state"><span><i class="el-icon-caret-bottom"></i></span>班级情况<span class="bold">国际班</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+            </div>
+          </div>  
     </div>
 </template>
 
@@ -24,6 +46,11 @@ export default {
     data() {
         return {
             searchContent:'',
+            allMajorname:[
+                {},
+                {},
+                {},
+            ]
         }
     },
     methods: {
@@ -35,6 +62,16 @@ export default {
 };
 </script>
 <style>
+    /*不同院校*/
+    .diffeCollege [class*=" el-icon-"], [class^=el-icon-] {
+        width: 19px;
+        height: 19px;
+        margin-right: 8px;
+    }
+    .diffeCollege .el-col-8 {
+        width: 1280px;
+    }
+
 
     .selectCollegeSearch .el-input__inner {
         border-radius: 60px;
@@ -42,16 +79,173 @@ export default {
     }
 </style>
 <style scoped>
+    /*不同院校——院校信息*/
+    .moneyType p,.state,.interval {
+        font-size: 14px;
+        color: #6e6e6e;
+    }
+    .interval {
+        margin: 10px 0 10px 0;
+    }
+    .money {
+        margin: 0 44px 0 11px;
+    }
+    .bold {
+        margin-left: 18px;
+    }
+    .type {
+        margin-left: 14px;
+    }
+    .money,.type,.bold {
+        font-size: 14px;
+        font-weight: bold;
+        color: #6e6e6e;
+    }
+    .international {
+        height: 19px;
+        margin-bottom: 21px;
+        font-size: 18px;
+        color: #009fa0;
+    }
+    .moneyType {
+        display: flex;
+    }
+    .specificInform {
+        width: 375px;
+        height: 140px;
+        padding: 30px 0 30px 25px;
+        margin: 0 10px 10px 0; 
+        background-color: #f5f5f5; 
+        border-radius: 5px;
+    }
+    .forY {
+        display: flex;
+        width: 1230px;
+    }
+    .collegeInform {
+        /*display: flex;*/
+    }
+    .diffeCollege {
+        margin: 0 auto;
+        width: 1280px;
+    }
+    .buttonCollege {
+        width: 1280px;
+        height: 2px;
+        background-color: #009fa0;
+        border: solid 3px #000000;
+        opacity: 0.06;
+        margin: 50px auto;
+    }
+
+    /*不同院校——图片信息*/
+    .collegePicture {
+        /*width: 1280px;*/
+        /*border-bottom: 1px solid red;*/
+    }
+
+
+
     .selectCollegeSearch {
         width: 407px;
         margin: 49px 0 44px;
     }
     .selectCollege {
-        width: 1280px;
+        width: 1260px;
         margin: 0 auto;
-        /*background-color: #f5f5f5;*/
-        /*display: flex;*/
-        /*flex-direction: column ;*/
-        /*align-items:center;*/
+    }
+
+
+    /** PC **/
+    @media only screen and (min-width: 1024px) and (max-width:1300px) {
+        .specificInform {
+             width: 720px;
+             margin: 0 0 10px 80px;
+        }
+        .forY {
+            flex-direction:column;
+            justify-content:center;
+        }
+        .buttonCollege {
+        }
+        .diffeCollege {
+            width: 100%;
+            overflow:hidden; 
+        }
+        .diffeCollege .el-col-8{
+            width: 1000px;
+            overflow:hidden;
+            margin-left: 10px;
+        }
+
+        .selectCollegeSearch {
+            margin-left: 10px;
+        }
+        .selectCollege {
+        }
+    }
+    /** iPad **/
+    @media only screen and (min-width: 768px) and (max-width: 1023px) {
+        .specificInform {
+            width: 658px;
+            margin-right: 25px;
+        }
+        .forY {
+            flex-direction:column;
+            justify-content:center;
+        }
+        .buttonCollege {
+        }
+        .diffeCollege {
+            width: 100%;
+            overflow:hidden; 
+        }
+        .diffeCollege .el-col-8{
+            width: 720px;
+            overflow:hidden;
+            margin-left: 20px;
+        }
+
+        .selectCollegeSearch {
+        }
+        .selectCollege {
+            width: 700px;
+        }
+    }
+    /** iPhone **/
+    @media only screen and (min-width: 320px) and (max-width: 767px) {
+        .teach {
+            margin-top: 10px;
+        }
+        .moneyType {
+            flex-direction:column ;
+        }
+        .specificInform {
+            width: 220px;
+            height: 150px;
+            padding: 30px 25px;
+            /*margin-bottom: 10px;*/
+        }
+        .diffeCollege .el-col-8 {
+            width: 1000px;
+            margin-left: 20px;
+        }
+        .forY {
+            width: 800px;
+            flex-direction:column;
+            justify-content:center;
+            align-items:center;
+        }
+        .diffeCollege {
+            width: 1000px;
+        }
+
+        .selectCollegeSearch {
+            /*width: 90%;*/
+            margin-left: 20px;
+        }
+        .selectCollege {
+            /*width: 100%;*/
+        }
     }
 </style>
