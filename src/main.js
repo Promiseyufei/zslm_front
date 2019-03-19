2cc3e8f5a0f44654daeb379b975eb4705e40d8f9
jinzhao <1743728692@qq.com>
2019-03-19 17:29:39 +0800

    修改富文本引用文件

--- src/main.js
+++ src/main.js
@@ -45,7 +45,7 @@ window.WangEditor = WangEditor
 
 
 import '../static/UE/ueditor.config.js'
-import '../static/UE/ueditor.all.min.js'
+import '../static/UE/ueditor.all.js'
 import '../static/UE/lang/zh-cn/zh-cn.js'
 import '../static/UE/ueditor.parse.min.js'
 
