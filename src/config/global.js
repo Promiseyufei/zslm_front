296f1ce773ad061b8f58418a026d7203fc37deab
wuyufei <895457187@qq.com>
2019-04-28 14:37:46 +0800

    test

--- src/config/global.js
+++ src/config/global.js
@@ -5,9 +5,9 @@
 // const longUrl = 'http://www.zslm.com'
 // const excelUrl = "http://www.zslm.com"
 
-const testUrl = 'http://www.mbahelper.cn:8889'
-const longUrl = 'http://www.mbahelper.cn:8889'
-const excelUrl = "http://www.mbahelper.cn:8889"
+const testUrl = 'http://129.211.2.78:82'
+const longUrl = 'http://129.211.2.78:82'
+const excelUrl = "http://129.211.2.78:82"
 
 
 var vvv = this
